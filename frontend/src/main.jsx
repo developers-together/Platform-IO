abc644246dbde36f7449ea16e507412124fa6160
Shehab <saohm56@gmail.com>
2025-03-15 00:05:17 +0200

    fixed frontend problem

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App"; // Import your App component
+import App from "./app"; // Import your App component
 
 // Mount the app to the root element
 const root = ReactDOM.createRoot(document.getElementById("root"));
