415a267ecf0591db7b79fd1dafead9d620697596
ShehabMohamedGamal <shehabmohamedgamal1@gmail.com>
2026-03-02 01:33:26 +0200

    configured Platform-IO for the big refactor

--- frontend/src/main.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./app"; // Import your App component
-
-// Mount the app to the root element
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
